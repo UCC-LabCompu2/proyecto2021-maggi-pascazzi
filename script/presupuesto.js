9fab097fb8fa002d6e18d20c6bb303ac3cffea18
fmaggi <franciscomaggi1999@gmail.com>
2021-06-09 11:10:37 -0300

    com

--- script/presupuesto.js
+++ script/presupuesto.js
@@ -1,3 +1,11 @@
+var usos = {
+    "game": [0.3, 0.4, 0.15, 0.15],
+    "diseno": [0.3, 0.4, 0.2, 0.1],
+    "edicion": [0.25, 0.4, 0.25, 0.10],
+    "programming": [0.4, 0.1, 0.25, 0.25],
+    "ligero": [0.4, 0.1, 0.1, 0.4]
+}
+
 var cpus = {
     tier1: {
         "Intel i3": {precio: 1, rendimiento: 1},
@@ -31,7 +39,7 @@ var gpus = {
     }
 }
 
-var ram = {
+var rams = {
     "4gb": {},
     "8gb": {},
     "16gb": {},
@@ -39,7 +47,7 @@ var ram = {
     "64gb": {}
 }
 
-var ssd = {
+var ssds = {
     "128gb": {},
     "256gb": {},
     "512gb": {},
@@ -47,16 +55,52 @@ var ssd = {
     "2tb": {}
 }
 
-var usuario;
-var presupuesto;
-var uso;
+/*
+*
+* @method window.onload
+*/
 window.onload = function () {
     let params = new URLSearchParams(location.search);
-    usuario = params.get("usuario");
-    presupuesto = params.get("presupuesto");
-    uso = params.get("uso");
+    let usuario = params.get("usuario");
+    let presupuesto = params.get("presupuesto");
+    let uso = params.get("uso");
 
     document.getElementById("header").innerHTML = usuario + " ya tenemos tu PC!"
+
+    ubicarComponentes(presupuesto, uso);
+}
+
+/*
+*
+* @method ubicarComponentes
+* @param {number} presupuesto - presupuesto del usuario
+* @param {string} uso - uso que el usuario le va a dar a la PC
+*/
+function ubicarComponentes(presupuesto, uso) {
+    let porcentajes = usos[uso];
+    let p_cpu = porcentajes[0];
+    let p_gpu = porcentajes[1];
+    let p_ram = porcentajes[2];
+    let p_ssd = porcentajes[3];
+    let resultados = [];
+
+    for (const key of Object.keys(cpus)) {
+        if (p_cpu * presupuesto <= key) {
+            resultados.push(key);
+
+            var cpu = Object.keys(cpus[key])[0]
+            document.getElementById("cpu").innerHTML = cpu;
+        }
+    }
+
+    for (const key of Object.keys(gpus)) {
+        if (p_cpu * presupuesto <= key) {
+            resultados.push(key);
+
+            var gpu = Object.keys(gpus.key)[0]
+            document.getElementById("gpu").innerHTML = gpu;
+        }
+    }
 }
 
 function cambiar(componente, indice) {
